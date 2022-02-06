68a9ddbb483f51bb07b8cdb68cbaa18862fb5f30
Geunho Jang <39117502+Rootjang92@users.noreply.github.com>
2022-02-06 10:55:59 +0900

    Router 초기 설정

---
+++ src/components/Router.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+
+import Home from "../routes/Home";
+import Auth from "../routes/Auth";
+import Profile from "../routes/Profile";
+import EditProfile from "../routes/EditProfile";
+
+const AppRouter = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState();
+
+  return (
+    <Router>
+      <Switch>
+        {isLoggedIn ? (
+          <>
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </>
+        ) : (
+          <>
+            <Route exact path="/">
+              <Auth />
+            </Route>
+          </>
+        )}
+      </Switch>
+    </Router>
+  );
+};
+
+export default AppRouter;
